1f6ddbb76453cc61c4abe4accac9478097cf4f08
luiz fernando <luiz123jfmg@gmail.com>
2022-01-24 17:18:07 -0300

    Adicionando o HTML e CSS da calculadora

--- src/app/calculadora/calculadora.module.ts
+++ src/app/calculadora/calculadora.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalculadoraComponent } from './components';
+import { CalculadoraService } from './services';
 
 
 
@@ -13,6 +14,9 @@ import { CalculadoraComponent } from './components';
   ],
   imports: [
     CommonModule
+  ],
+  providers: [
+    CalculadoraService //registrando como um provedor de serviços para o Angular
   ]
 })
 
