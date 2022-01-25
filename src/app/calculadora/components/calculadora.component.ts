cf42390203d0f2a8a2a9c9c1c80dead2b46b18fc
luiz fernando <luiz123jfmg@gmail.com>
2022-01-24 21:06:24 -0300

    Criando métodos do componente calculadora

--- src/app/calculadora/components/calculadora.component.ts
+++ src/app/calculadora/components/calculadora.component.ts
@@ -9,11 +9,67 @@ import { CalculadoraService } from '../services';
 })
 export class CalculadoraComponent implements OnInit {
 
+  private numero1: string;
+  private numero2: string;
+  private resultado: number;
+  private operacao: string;
+
   //injeção de dependencias (injetando o serviço de calculadora)
   constructor(private calculadoraService: CalculadoraService) { }
 
   ngOnInit(): void {
-    
+    this.limpar();
   }
+
+  /**
+   * Inicializar todos os operadores para valoers padrão
+   * @return void
+   */
+  limpar(): void {
+    this.numero1 = '0';
+    this.numero2 = null;
+    this.resultado = null;
+    this.operacao = null;
+  }
+
+  /**
+   * Adiciona o número selecionado para o cálculo posteriormente
+   * 
+   * @param string numero 
+   * @return void
+   */
+  adicionarNumero(numero: string): void {
+    if(this.operacao === null){
+      this.numero1 = this.concatenarNumero(this.numero1, numero);
+    }else{
+      this.numero2 = this.concatenarNumero(this.numero2, numero);
+    }
+  }
+
+
+  /**
+   * Retorna o valor concatenado. Trata o separador decimal
+   * 
+   * @param numAtual 
+   * @param numConcat 
+   * @return string
+   */
+  concatenarNumero(numAtual: string, numConcat: string): string {
+    if(numAtual === '0' || numAtual === null){
+      numAtual = '';
+    }
+
+    if(numConcat === '0' || numConcat === null){
+      numConcat = '';
+    }
+
+    if(numConcat === '.' && numAtual.indexOf('.') > -1) {
+      return numAtual;
+    }
+
+    return numAtual + numConcat
+  }
+
+
 
 }
