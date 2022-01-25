2c5b17a99861487a1f8707bf2e0b426032933d85
luiz fernando <luiz123jfmg@gmail.com>
2022-01-25 07:36:48 -0300

    Implementando o component CalculadoraComponent

--- src/app/calculadora/components/calculadora.component.ts
+++ src/app/calculadora/components/calculadora.component.ts
@@ -39,9 +39,9 @@ export class CalculadoraComponent implements OnInit {
    * @return void
    */
   adicionarNumero(numero: string): void {
-    if(this.operacao === null){
+    if (this.operacao === null) {
       this.numero1 = this.concatenarNumero(this.numero1, numero);
-    }else{
+    } else {
       this.numero2 = this.concatenarNumero(this.numero2, numero);
     }
   }
@@ -55,20 +55,73 @@ export class CalculadoraComponent implements OnInit {
    * @return string
    */
   concatenarNumero(numAtual: string, numConcat: string): string {
-    if(numAtual === '0' || numAtual === null){
+    if (numAtual === '0' || numAtual === null) {
       numAtual = '';
     }
 
-    if(numConcat === '0' || numConcat === null){
+    if (numConcat === '0' || numConcat === null) {
       numConcat = '';
     }
 
-    if(numConcat === '.' && numAtual.indexOf('.') > -1) {
+    if (numConcat === '.' && numAtual.indexOf('.') > -1) {
       return numAtual;
     }
 
     return numAtual + numConcat
   }
+
+
+  /**
+   * 
+   * @param string operacao 
+   * @return void
+   */
+  definirOperacao(operacao: string): void {
+    if (operacao === null) {
+      this.operacao = operacao;
+      return;
+    }
+    if (this.numero2 !== null) {
+      this.resultado = this.calculadoraService.calcular(
+        parseFloat(this.numero1),
+        parseFloat(this.numero2),
+        this.operacao
+      );
+      this.operacao = operacao;
+      this.numero1 = this.resultado.toString();
+      this.numero2 = null;
+      this.resultado = null;
+    }
+  }
+
+  /**
+   * Realiza o calculo
+   * @return void
+   */
+  calcular(): void {
+    if (this.numero2 === null) {
+      return;
+    }
+    this.resultado = this.calculadoraService.calcular(parseFloat(this.numero1), parseFloat(this.numero2), this.operacao);
+  }
+
+
+  /**
+   * Retorna o valor que será exibido na tela da calculadora
+   * @return string
+   */
+  get display(): string { // "GET" é um recurso do typescript, que vai gerar um atributo de classe chamado "display"
+    if(this.resultado !== null) {
+      return this.resultado.toString();
+    }
+    if(this.numero2 !== null){
+      return this.numero2;
+    }
+    return this.numero1;
+  }
+
+
+
 
 
 
